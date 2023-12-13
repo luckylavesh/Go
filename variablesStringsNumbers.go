352c99732e525c38d76d94a50e52a590c01aa991
Lavesh Soni <78368523+luckylavesh@users.noreply.github.com>
2023-12-13 17:13:42 +0530

    Updated

---
+++ variablesStringsNumbers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+   var itsname string = "Lavesh"
+   var two string 
+   fmt.Println(itsname, two)
+
+   //another way to declare
+   itssname := "Soni"
+   fmt.Println(itssname)
+
+   //for integers
+   var one = 204
+   atwo := 40
+   fmt.Println(one, atwo)
+   //same for float as well
+}
