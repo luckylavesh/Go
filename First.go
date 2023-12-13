a26eaccb805314cb8507180ae1aae2b4a13bcacd
luckylavesh <lsoni3446@gmail.com>
2023-12-13 16:57:43 +0530

    first commit

---
+++ First.go
@@ -0,0 +1,8 @@
+package main
+//fmt is standard library for formatting strings and printing messages to console
+import "fmt"
+//function name will start with func
+//only one main function will be there in every file
+func main(){
+	fmt.Println("Hello")
+}
