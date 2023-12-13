1dff8ed9d53a66c9a3588e9f1eb854d343a0aad5
Lavesh Soni <78368523+luckylavesh@users.noreply.github.com>
2023-12-13 17:56:05 +0530

    Add files via upload
    Learned

---
+++ PrintingFormatting.go
@@ -0,0 +1,13 @@
+package main
+import"fmt"
+func main(){
+	//for printing
+	mstring := "lavesh"
+	fmt.Print("lavesh")
+    //for printing on new line we have two methods
+	fmt.Print("soni \n")
+	fmt.Println("automatic new line")
+	//to use variable name in print
+	fmt.Println("My name is" ,mstring)
+
+}
